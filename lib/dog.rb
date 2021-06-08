6a977c285a9147d96b212bb46324c1cadd372bd3
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-08 22:43:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,18 +15,14 @@ class Dog
   
   def self.print_all
     @@all.each do |dog|
-      puts dog.name
+      puts 
+      
     end
   end
   
   def save 
     @@all << self 
   end
-  
-  def self.clear_all
-    @@all = []
-  end
-  
   
   
 end
